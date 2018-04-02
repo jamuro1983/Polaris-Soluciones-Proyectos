76cab1e2ace3b1321b5aa19f96982deb417bd6e9
Jason Muñoz Rojas <jamuro@hotmail.com>
2018-04-02 17:33:06 -0600

    1

---
+++ WebRegionalismos/Clases Compartidas/VariablesGlobales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRegionalismos.Clases_Compartidas
+{
+    public class VariablesGlobales
+    {
+        #region Titulos y Encabezados
+            public const string  nombreSistema = "Regionalismos";
+            public const string descripcionSistema = "Sistema de mantenimiento de regionalismos";
+
+            public const string labelOpciones = "Opciones";
+        #endregion
+
+        #region Catalogo de Paises
+            public const string labelPais = "Sigla";
+            public const string labelDescripcionPais = "Descripción de País";
+        #endregion
+
+
+
+    }
+
+}
